0a1bc1fdfd0cc4d94a58d53d7577b6fb109431b2
Thomas Alves <thomas981930@hotmail.com>
2024-09-26 23:48:48 +0000

    Salvar

--- app/src/main/java/application/repository/CategoriaRepository.java
+++ app/src/main/java/application/repository/CategoriaRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import application.model.Categoria;
 
 public interface CategoriaRepository extends CrudRepository<Categoria, Long> {
+    
 
 }
